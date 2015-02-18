ef29a9c427989b3f426327fcf8231ae4000fc067
ysakmrkm <info@ysakmrkm.com>
2015-02-18 14:23:46 +0900

    Add password retype and validation to account info edit

--- public/js/account.js
+++ public/js/account.js
@@ -1,3 +1,5 @@
+var passwordCheck;
+
 $(function() {
   $(".dropdown-button").dropdown({
     hover: false,
@@ -5,6 +7,24 @@ $(function() {
     alignment: 'right',
     belowOrigin: true
   });
+  $('#password').keyup(function() {
+    if ($(this).val() !== '') {
+      return $('#password-re').attr('required', 'required');
+    } else {
+      return $('#password-re').removeAttr('required', 'required');
+    }
+  });
+  return $('#password-re').keyup(function() {
+    return passwordCheck($(this).get(0));
+  });
 });
+
+passwordCheck = function(input) {
+  if (input.value !== document.getElementById('password').value) {
+    input.setCustomValidity('Re type password is no match.');
+  } else {
+    input.setCustomValidity('');
+  }
+};
 
 //# sourceMappingURL=account.js.map
--- src/cs/account.coffee
+++ src/cs/account.coffee
@@ -6,4 +6,20 @@ $ ->
     belowOrigin: true
   )
 
+  $('#password').keyup( ->
+    if $(this).val() isnt ''
+      $('#password-re').attr('required', 'required')
+    else
+      $('#password-re').removeAttr('required', 'required')
+  )
+
+  $('#password-re').keyup( ->
+    passwordCheck($(this).get(0))
+  )
+
+passwordCheck = (input) ->
+  if input.value isnt document.getElementById('password').value
+    input.setCustomValidity('Re type password is no match.')
+  else
+    input.setCustomValidity('')
   return
